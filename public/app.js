520f1dab61fd3619e72ea360d4bb9228b6909802
Brandon Rider <brandonrider@Brandons-MacBook-Pro.local>
2016-07-04 12:10:02 -0600

    message

--- public/app.js
+++ public/app.js
@@ -1,1 +1,27 @@
-angular.module('myApp', []);
+angular.module('myApp', ['ui.router'])
+.config(function($stateProvider, $urlRouterProvider){
+$stateProvider
+  .state('home', {
+    templateUrl: './views/home.html',
+    url: '/'
+  })
+
+  .state('tma-2', {
+    templateUrl: './views/tma2tmpl.html',
+    url: '/headphones/tma-2'
+  })
+
+  .state('order', {
+    templateUrl: './views/order.html',
+    url: '/order'
+  })
+
+  .state('cart', {
+    templateUrl: './views/cart.html',
+    url: '/cart'
+  })
+
+$urlRouterProvider
+  .otherwise('/')
+
+});
